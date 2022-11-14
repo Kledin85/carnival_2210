adf8f52e27d088d072ef61ed962f9c79bbba707a
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-14 10:10:21 -0600

    Write a test to see if Visitor has preferences

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,12 +2,18 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
 
-  it "exists with attributes" do
+  it 'exists with attributes' do
     visitor1 = Visitor.new('Bruce', 54, '$10')
 
     expect(visitor1).to be_a(Visitor)
     expect(visitor1.name).to eq('Bruce')
     expect(visitor1.height).to eq(54)
     expect(visitor1.spending_money).to eq('$10')
+  end
+
+  it 'has preferences' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1.preferences).to eq([])
   end
 end
