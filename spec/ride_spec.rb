48f9ea6cf97278dcc9a66a156a623464991cd77f
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-14 10:37:29 -0600

    Write a test for the rider log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe Ride do
 
     expect(ride1.total_revenue).to eq(0)
   end
+
+  it 'has a rider log' do
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+    expect(ride1.rider_log).to eq ({})
+  end
+
+  xit 'can board a visitor' do
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+  end
 end
