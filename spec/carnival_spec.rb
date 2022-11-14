00da31172ccc26271daff2a0aab783352c39add6
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-14 12:16:10 -0600

    Write a test for total revenue at the carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -30,6 +30,36 @@ RSpec.describe Carnival do
     carnival.add_ride(ride3)
 
     expect(carnival.rides).to eq([ride1, ride2, ride3])
+  end
+
+  it 'can calcualate total revenue from all rides' do
+    carnival = Carnival.new('May 1st - May 8th')
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    carnival.add_ride(ride3)
+
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+
+    ride2.board_rider(visitor1)
+    ride2.board_rider(visitor2)
+
+    ride3.board_rider(visitor3)
+    # require 'pry' ; binding.pry
+    expect(carnival.total_revenue).to eq(15)
+
 
 
   end
