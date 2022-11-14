11209684f864ae6a181d53e34a49f555ca78dc9d
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-14 10:07:49 -0600

    Have the test to see if Vistor exists with attributes passing

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,11 +2,12 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
 
-  it "exists with attributes"
+  it "exists with attributes" do
     visitor1 = Visitor.new('Bruce', 54, '$10')
 
     expect(visitor1).to be_a(Visitor)
     expect(visitor1.name).to eq('Bruce')
     expect(visitor1.height).to eq(54)
     expect(visitor1.spending_money).to eq('$10')
+  end
 end
